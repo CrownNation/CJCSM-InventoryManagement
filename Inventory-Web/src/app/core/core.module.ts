eb413fdddc79648f89f580b810bb08000d6f8616
Tyson Villeneuve <tyson@crownnation.com>
2024-10-01 12:22:36 -0600

    Update in progress

--- Inventory-Web/src/app/core/core.module.ts
+++ Inventory-Web/src/app/core/core.module.ts
@@ -17,41 +17,32 @@ import { metaReducers, reducers } from '../store/core.state';
 import { LoginComponent } from '../auth/login/login.component';
 import { RackEffects } from '../store/rack/rack.effects';
 import { TallyEffects } from '../store/tally/tally.effects';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TallyService } from './services/tally-service/tally.service';
 import { CustomerService } from './services/customer-service/customer.service';
 import { CustomerEffects } from '../store/customer/customer.effects';
 import { PipeEffects } from '../store/pipe/pipe.effects';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-@NgModule({
-  declarations: [ 
-  ],
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    AuthModule,
-    MatSnackBarModule,
-
-    // ngrx
-    StoreModule.forRoot(reducers, { metaReducers }),
-    StoreRouterConnectingModule.forRoot(),
-    EffectsModule.forRoot([
-      RackEffects,
-      TallyEffects,
-      CustomerEffects,
-      PipeEffects
-    ]),
-    environment.production ? [] : StoreDevtoolsModule.instrument({
-      name: 'CJCSM Inventory'
-    }),
-  ],
-  providers:[ RackService, TallyService, CustomerService ],
-  exports: [
-    HttpClientModule,
-    LoginComponent
-  ]
-})
+@NgModule({ declarations: [],
+    exports: [
+        HttpClientModule,
+        LoginComponent
+    ], imports: [CommonModule,
+        AuthModule,
+        MatSnackBarModule,
+        // ngrx
+        StoreModule.forRoot(reducers, { metaReducers }),
+        StoreRouterConnectingModule.forRoot(),
+        EffectsModule.forRoot([
+            RackEffects,
+            TallyEffects,
+            CustomerEffects,
+            PipeEffects
+        ]),
+        environment.production ? [] : StoreDevtoolsModule.instrument({
+            name: 'CJCSM Inventory'
+        })], providers: [RackService, TallyService, CustomerService, provideHttpClient(withInterceptorsFromDi())] })
 
 export class CoreModule {
   constructor(
